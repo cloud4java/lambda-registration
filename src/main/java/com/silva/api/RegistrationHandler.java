6b7c41689dca9f6d24be427f46a3d52057275619
Dorival <dorival.querino@gmail.com>
2019-01-27 11:25:04 -0200

    Initial commit.

---
+++ src/main/java/com/silva/api/RegistrationHandler.java
@@ -0,0 +1,31 @@
+package com.silva.api;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class RegistrationHandler implements RequestHandler<RegistrationHandler.Request, RegistrationHandler.Response>{
+
+    public Response handleRequest(Request request, Context context) {
+        String greetings =  String.format("Hello, %s %s", request.firstName, request.lastName);
+        LambdaLogger logger = context.getLogger();
+        logger.log("############ Log output: " + greetings);
+        return new Response(request.firstName, request.lastName);
+    }
+
+    static class Request{
+        public String lastName;
+        public String firstName;
+    }
+
+
+    static class Response{
+        public String lastName;
+        public String firstName;
+
+        public Response(String lastName, String firstName) {
+            this.lastName = lastName;
+            this.firstName = firstName;
+        }
+    }
+}
